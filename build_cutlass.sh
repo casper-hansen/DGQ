22a5edb4a47a4974158d2144dd03ff5576e392b2
Casper Hansen <casperbh.96@gmail.com>
2023-12-30 12:46:37 +0000

    Update cutlass script

--- build_cutlass.sh
+++ build_cutlass.sh
@@ -1,6 +1,6 @@
-export CUDACXX=/home/zlm/cuda-12.1/cuda-12.1/bin/nvcc
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
+export CUDACXX=/usr/local/cuda/bin/nvcc
+# export CC=/usr/bin/gcc
+# export CXX=/usr/bin/g++
 cd submodules/cutlass
 rm -rf build
 mkdir -p build && cd build
